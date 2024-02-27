d4c687a875ae880d587ece16561b5230c9b10c8f
daniilsonufrijuks <do00492@rvt.lv>
2024-02-27 10:47:00 +0000

    basic methods info

---
+++ src/Person.java
@@ -0,0 +1,23 @@
+public class Student {
+    private String name;
+    private String lastname;
+    private String email;
+    private String pesoncode;
+    private String email;
+
+    public void SetName(String Newname) {
+        this.name = Newname;
+    }
+
+    public void SetLastName(String Newlastname) {
+        this.lastname = Newlastname;
+    }
+
+    public void SetEmail(String Newemail) {
+        this.email = Newemail;
+    }
+
+    public void SetPersonCode(String Newpersoncode) {
+        this.pesoncode = Newpersoncode;
+    }
+}

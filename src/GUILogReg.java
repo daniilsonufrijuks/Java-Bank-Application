b3770c8136ba5f319bffb88d6803dd8b4a3a4d36
daniilsonufrijuks <do00492@rvt.lv>
2024-02-28 12:48:25 +0000

    gui problems

--- src/GUILogReg.java
+++ src/GUILogReg.java
@@ -1,27 +1,11 @@
-import javax.swing.*;
-import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-import java.awt.FlowLayout;  
-import javax.swing.JButton;  
-import javax.swing.JFrame;  
-import javax.swing.JLabel;  
-import javax.swing.JPanel;  
+import javax.swing.*;    
 
 public class GUILogReg {  
     public static void main(String s[]) {  
-        JFrame frame = new JFrame("JFrame Example");  
-        JPanel panel = new JPanel();  
-        panel.setLayout(new FlowLayout());  
-        JLabel label = new JLabel("JFrame By Example");  
-        JButton button = new JButton();  
-        button.setText("Button");  
-        panel.add(label);  
-        panel.add(button);  
-        frame.add(panel);  
-        frame.setSize(200, 300);  
-        frame.setLocationRelativeTo(null);  
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);  
-        frame.setVisible(true);  
+        JFrame frame = new JFrame();
+        frame.setSize(150,150);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setTitle("Test Frame");
+        frame.setVisible(true); 
     }  
 }  

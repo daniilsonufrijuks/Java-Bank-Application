40f1ffa4c70323692c65ecffb9ddda91eaa19707
daniilsonufrijuks <do00492@rvt.lv>
2024-02-28 12:06:23 +0000

    reg log

--- src/Registration.java
+++ src/Registration.java
@@ -1,5 +1,10 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileReader;
 import java.io.FileWriter;   // Import the FileWriter class
 import java.io.IOException;  // Import the IOException class to handle errors
+import java.util.Scanner;
 
 static class Registration{      // Registration static class
 
@@ -15,5 +20,45 @@ static class Registration{      // Registration static class
         }
         // i made some code
     }
+
+    // try {
+    //         File myObj = new File("person.csv");
+    //         Scanner myReader = new Scanner(myObj);
+    //         int line_id = 1;
+    //         while (myReader.hasNextLine()) {
+    //             String data = myReader.nextLine();
+    //             System.out.println(line_id + " " + data);
+    //             line_id++;
+    //         }
+    //         Scanner myObjremo = new Scanner(System.in);  // Create a Scanner object 3d
+    //         System.out.println("Enter line to remove: ");
+    //         String userinput_line = String.valueOf(myObjremo.nextLine());
+            
+    //         String lineToRemove = userinput_line;
+
+    //         File tempFile = new File("person2.csv");
+    //         BufferedWriter writer = new BufferedWriter(new FileWriter(tempFile));
+
+    //         BufferedReader reader = new BufferedReader(new FileReader(fileName));
+    //         String currentLine;
+
+    //         while ((currentLine = reader.readLine()) != null) {
+    //             if (!currentLine.equals(lineToRemove)) {
+    //                 writer.write(currentLine + System.getProperty("line.separator"));
+    //             }
+    //         }
+    //         reader.close();
+    //         writer.close();
+
+    //         File oldFile = new File(fileName);
+    //         oldFile.delete();
+    //         tempFile.renameTo(oldFile);
+
+    //         System.out.println("String " + lineToRemove + " deleted from file " + fileName);
+
+    //     } catch (IOException ex) {
+    //         System.out.println("Error " + fileName);
+    //         ex.printStackTrace();
+    //     }
 
 }

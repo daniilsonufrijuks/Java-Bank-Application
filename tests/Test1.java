2bf1f0f6b06a933aba917250fb2b9a6a69cb9840
Danila Vasiljevs DP2-1 <dv00710@rvt.lv>
2024-02-28 12:42:56 +0000

    WHATTHEFUCKISWITHTESTS

---
+++ tests/Test1.java
@@ -0,0 +1,14 @@
+package tests;
+import static org.junit.Assert.assertEquals;
+import org.junit.Test;  // test library
+import src.Registration.java;
+import Person.java;
+
+public class Test1{
+
+    @Test
+    public void RegistrationTest(){
+        Person person = new Person()
+    }
+    
+}

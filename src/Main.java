debe53a11dd49ad6748de116126d31e911474c41
daniilsonufrijuks <do00492@rvt.lv>
2024-03-01 18:21:52 +0200

    login algorithm

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,4 @@
+import javax.swing.ImageIcon;
 import javax.swing.JFrame;
 
 public class Main {
@@ -5,6 +6,8 @@ public class Main {
         // gui start
         GUILogReg frame = new GUILogReg();
         frame.setTitle("MONOLIT Bank");
+        // ImageIcon icon = new ImageIcon("icon.png"); // Путь к вашей иконке
+        // setIconImage(icon.getImage());
         frame.setVisible(true);
         frame.setBounds(10, 10, 1200, 750);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);

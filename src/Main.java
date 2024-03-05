bcb93768dcfccb542fa50587c722f62726f095ab
daniilsonufrijuks <do00492@rvt.lv>
2024-03-05 12:20:37 +0200

    main start window then

--- src/Main.java
+++ src/Main.java
@@ -4,14 +4,14 @@ import javax.swing.JFrame;
 public class Main {
     public static void main(String[] args) {
         // gui start
-        StartWindow stw = new StartWindow();
-        stw.setTitle("MONOLIT Bank");
-        // ImageIcon icon = new ImageIcon("icon.png");
-        // setIconImage(icon.getImage());
-        stw.setVisible(true);
-        stw.setBounds(10, 10, 500, 750);
-        stw.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        stw.setResizable(false);
+        // StartWindow stw = new StartWindow();
+        // stw.setTitle("MONOLIT Bank");
+        // // ImageIcon icon = new ImageIcon("icon.png");
+        // // setIconImage(icon.getImage());
+        // stw.setVisible(true);
+        // stw.setBounds(10, 10, 500, 750);
+        // stw.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        // stw.setResizable(false);
 
         // log window
         GUILogReg frame = new GUILogReg();

4356dcd92a64199936b4b21ef43766f71b716034
daniilsonufrijuks <do00492@rvt.lv>
2024-03-06 13:09:12 +0200

    sleep start window

--- src/Main.java
+++ src/Main.java
@@ -1,18 +1,24 @@
 import javax.swing.ImageIcon;
 import javax.swing.JFrame;
 
+
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         // gui start
         StartWindow stw = new StartWindow();
         stw.setTitle("MONOLITH Bank");
         // ImageIcon icon = new ImageIcon("icon.png");
         // setIconImage(icon.getImage());
         stw.setVisible(true);
+        // int x = (screenSize.width - frame.getWidth()) / 2;  
+        // int y = (screenSize.height - frame.getHeight()) / 2;  
+        // //Set the new frame location  
+        // stw.setLocation(x, y);  
         stw.setBounds(10, 10, 400, 550);
         stw.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         stw.setResizable(false);
 
+        Thread.sleep(3000);
         // log window
         GUILogReg frame = new GUILogReg();
         frame.setTitle("MONOLITH Bank");

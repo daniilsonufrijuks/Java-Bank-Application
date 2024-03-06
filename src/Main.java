b4e5e1e4c43c237af10b073f41163b04437b8405
daniilsonufrijuks <do00492@rvt.lv>
2024-03-06 13:00:52 +0200

    MONOLITH

--- src/Main.java
+++ src/Main.java
@@ -4,18 +4,18 @@ import javax.swing.JFrame;
 public class Main {
     public static void main(String[] args) {
         // gui start
-        // StartWindow stw = new StartWindow();
-        // stw.setTitle("MONOLIT Bank");
-        // // ImageIcon icon = new ImageIcon("icon.png");
-        // // setIconImage(icon.getImage());
-        // stw.setVisible(true);
-        // stw.setBounds(10, 10, 500, 750);
-        // stw.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        // stw.setResizable(false);
+        StartWindow stw = new StartWindow();
+        stw.setTitle("MONOLITH Bank");
+        // ImageIcon icon = new ImageIcon("icon.png");
+        // setIconImage(icon.getImage());
+        stw.setVisible(true);
+        stw.setBounds(10, 10, 400, 550);
+        stw.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        stw.setResizable(false);
 
         // log window
         GUILogReg frame = new GUILogReg();
-        frame.setTitle("MONOLIT Bank");
+        frame.setTitle("MONOLITH Bank");
         // ImageIcon icon = new ImageIcon("icon.png"); 
         // setIconImage(icon.getImage());
         frame.setVisible(true);
--- tests/RegexTest.java
+++ tests/RegexTest.java
@@ -1,5 +1,10 @@
 package tests;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.Test;
+
+
 public class RegexTest {
     
 }

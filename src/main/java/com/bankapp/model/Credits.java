e81915fbbabed9686e140f6f78c74e0ec1285103
daniilsonufrijuks <do00492@rvt.lv>
2024-03-25 12:44:57 +0200

    working with credits

--- src/main/java/com/bankapp/model/Credits.java
+++ src/main/java/com/bankapp/model/Credits.java
@@ -1,5 +1,13 @@
 package model;
 
 public class Credits {
-    
+    private float total;
+    private float peryeartaxes;
+    private String description;
+
+    public Credits(float NewTotal, float NewPeryaertaxes, String Newdescription) {
+        this.total = NewTotal;
+        this.peryeartaxes= NewPeryaertaxes;
+        this.description = Newdescription;
+    }
 }

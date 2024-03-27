1ad4584ec2ce265c0d53ede2c9d8950f2bc0f91a
daniilsonufrijuks <do00492@rvt.lv>
2024-03-27 09:08:12 +0200

    new gen credit gen

--- src/main/java/com/bankapp/managers/CreditManager.java
+++ src/main/java/com/bankapp/managers/CreditManager.java
@@ -4,7 +4,7 @@ public class CreditManager {
     public float GenCredit(float money, float percents, int years) {
         float credit = 0.0f;
         if (money > 0) {
-            credit = money + (money * percents * years);
+            credit = (float) (money * Math.pow(1 + percents / 100, years));
             return credit;
         }
         return credit;

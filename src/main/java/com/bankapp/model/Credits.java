105d3c411d3b3ff069d2413a0cb5ea5c87ebf765
daniilsonufrijuks <do00492@rvt.lv>
2024-04-06 15:51:39 +0300

    cradits class file updated

--- src/main/java/com/bankapp/model/Credits.java
+++ src/main/java/com/bankapp/model/Credits.java
@@ -2,13 +2,9 @@ package model;
 
 public class Credits {
     private float total;
-    private float peryeartaxes;
-    private String description;
     // Constructor
-    public Credits(float NewTotal, float NewPeryaertaxes, String Newdescription) {
+    public Credits(float NewTotal) {
         this.total = NewTotal;
-        this.peryeartaxes= NewPeryaertaxes;
-        this.description = Newdescription;
     }
 
     // Getters and Setters
@@ -21,21 +17,4 @@ public class Credits {
         this.total = NewTotal;
     }
 
-    public float getPeryeartaxes() {
-        return peryeartaxes;
-    }
-
-    public void setPeryeartaxes(float NewPeryaertaxes) {
-        this.peryeartaxes = NewPeryaertaxes;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String Newdescription) {
-        this.description = Newdescription;
-    }
-
-    
 }

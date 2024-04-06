f63b61c75f30286031f40c0d89ea2d8c80a76ebb
daniilsonufrijuks <do00492@rvt.lv>
2024-04-06 11:35:07 +0300

    python file for email sending

--- src/main/java/com/bankapp/controller/AutoSendEmailPY.py
+++ src/main/java/com/bankapp/controller/AutoSendEmailPY.py
@@ -2,36 +2,42 @@ from mailersend import emails
 from dotenv import load_dotenv
 import os
 
-load_dotenv()
+def send():
+    load_dotenv()
 
-mailer = emails.NewEmail(os.getenv('MAILERSEND_API_KEY'))
+    mailer = emails.NewEmail(os.getenv('mlsn.13b8ebc4ce704c2e43a1ead6dac9ef778ffe77b1291461ad8ecf8e1e481bfc64'))
 
-# define an empty dict to populate with mail values
-mail_body = {}
+    # define an empty dict to populate with mail values
+    mail_body = {}
 
-mail_from = {
-    "name": "Your Name",
-    "email": "your@domain.com",
-}
-
-recipients = [
-    {
-        "name": "Your Client",
-        "email": "your@client.com",
+    mail_from = {
+        "name": "...",
+        "email": "monolithabout@gmail.com",
     }
-]
 
-reply_to = {
-    "name": "Name",
-    "email": "reply@domain.com",
-}
+    recipients = [
+        {
+            "name": "Your Client",
+            "email": "kroshthebestfriend@gmail.com",
+        }
+    ]
 
-mailer.set_mail_from(mail_from, mail_body)
-mailer.set_mail_to(recipients, mail_body)
-mailer.set_subject("Hello!", mail_body)
-mailer.set_html_content("This is the HTML content", mail_body)
-mailer.set_plaintext_content("This is the text content", mail_body)
-mailer.set_reply_to(reply_to, mail_body)
+    # reply_to = {
+    #     "name": "Name",
+    #     "email": "reply@domain.com",
+    # }
 
-# using print() will also return status code and data
-mailer.send(mail_body)
+    mailer.set_mail_from(mail_from, mail_body)
+    mailer.set_mail_to(recipients, mail_body)
+    mailer.set_subject("Hello!", mail_body)
+    mailer.set_html_content("This is the HTML content", mail_body)
+    mailer.set_plaintext_content("This is the text content", mail_body)
+    # mailer.set_reply_to(reply_to, mail_body)
+
+    # using print() will also return status code and data
+    mailer.send(mail_body)
+
+def main():
+    send()
+
+main()

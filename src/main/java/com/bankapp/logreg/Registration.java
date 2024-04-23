fa23ad5ed65a83e84101c11bab5a1c332e258144
daniilsonufrijuks <do00492@rvt.lv>
2024-04-23 18:32:01 +0300

    new coments, details and sell btn works normaly now )

--- src/main/java/com/bankapp/logreg/Registration.java
+++ src/main/java/com/bankapp/logreg/Registration.java
@@ -1,8 +1,7 @@
 package logreg;
 
 
-import java.io.File;
-import java.io.FileReader;
+import java.io.File;  // Import the File class
 import java.io.FileWriter;   // Import the FileWriter class
 import java.io.IOException;  // Import the IOException class to handle errors
 import java.util.Scanner;
